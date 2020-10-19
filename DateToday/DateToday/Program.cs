3cd66e13246e64c465f3888181d2e056f2bed024
KashifRazzaq <70984854+KashifRazzaq@users.noreply.github.com>
2020-10-19 19:28:29 +1300

    Update Program.cs

--- DateToday/DateToday/Program.cs
+++ DateToday/DateToday/Program.cs
@@ -6,7 +6,7 @@ namespace DateToday
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Kashif Bhai");
+            Console.WriteLine("Hello Kashif Bhai G");
         }
     }
 }
